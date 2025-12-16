564b941fe14da9a7a0e2001dca9e3973226f058e
Adeyemi oluwatoyosi <adeyemitoyosi4251@gmail.com>
2025-12-16 16:22:50 +0100

    I deleted some code in line 13 and 14

--- eeprom_functions/eeprom_functions.ino
+++ eeprom_functions/eeprom_functions.ino
@@ -1,23 +1,4 @@
-#include "EEPROM_DATA.h"
-<<<<<<< HEAD
 
-SystemData data; //stores system
-
-void loadSystemData() {
-  EEPROM.get(0, data); //read system
-
-  if (strlen(data.userPassword) != 4) strcpy(data.userPassword, "0000");
-  if (strlen(data.adminPassword) != 4) strcpy(data.adminPassword, "4251");
-
-  if (data.systemMode > 5) data.systemMode = 0;// Reset invalid system mode
-  if (data.wrongAttempts > 10) data.wrongAttempts = 0;// Reset wrong attempts
-
-  EEPROM.put(0, data);
-}
-
-void saveSystemData() {
-  EEPROM.put(0, data);
-=======
 const int up =4;
 const int ap =4;
 const int  Da = 0;
@@ -35,5 +16,4 @@ void loadSystemData() {
 
 void saveSystemData() {
   EEPROM.put(Da , data);
->>>>>>> dc33c52 (I removed the magic fig and merged the eeprom files)
-}
+
